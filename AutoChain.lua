bbbdc3a6c277476237c4e313e887f6a8455517e9
Sigmanic <71888803+Sigmanic@users.noreply.github.com>
2022-04-07 21:56:33 +0700

    Fixed compatible with recorder and autostrat
    Added a new method to selected tower which will be used

--- AutoChain.lua
+++ AutoChain.lua
@@ -1,5 +1,9 @@
 local comm1,comm2,comm3
-local troops = {1,2,3}
+local troops = {
+	Commander1 = {1},
+	Commander2 = {2},
+	Commander3 = {3},
+}
 local library = loadstring(game:HttpGet("https://pastebin.com/raw/RZ49zwxL", true))()
 local w = library:CreateWindow('Auto Chain')
 local Toggle = w:Toggle('Toggle Auto Chain', {flag = "autochain"})
@@ -15,6 +19,31 @@ for i,v in pairs(game.CoreGui:GetDescendants()) do
 		comm3 = v
 	end
 end
+function SelectedTower(Tower,Value)
+	if Value == true then
+		if not troops["Commander1"][2] then
+			table.insert(troops["Commander1"],2,Tower)
+			Status(1).Text = "Commander 1: Selected"
+		elseif not troops["Commander2"][2] then
+			table.insert(troops["Commander2"],2,Tower)
+			Status(2).Text = "Commander 2: Selected"
+		elseif not troops["Commander3"][2] then
+			table.insert(troops["Commander3"],2,Tower)
+			Status(3).Text = "Commander 3: Selected"
+		end
+	elseif Value == false then
+		if troops["Commander1"][2] == Tower then
+			table.remove(troops["Commander1"],2)
+			Status(1).Text = "Commander 1: None"
+		elseif troops["Commander2"][2] == Tower then
+			table.remove(troops["Commander2"],2)
+			Status(2).Text = "Commander 2: None"
+		elseif troops["Commander3"][2] == Tower then
+			table.remove(troops["Commander3"],2)
+			Status(3).Text = "Commander 3: None"
+		end
+	end
+end
 function Status(Tower)
 	if Tower == 1 then
 		return comm1
@@ -27,69 +56,51 @@ end
 for i,v in pairs(game:GetService("Workspace").Towers:GetChildren()) do
 	if v.Replicator:GetAttribute("OwnerName") then 
 		if v.Replicator:GetAttribute("OwnerName") == game.Players.LocalPlayer.DisplayName and v.Replicator:GetAttribute("Type") == "Commander" then
-			v.Name = troops[1]
-			Status(tonumber(v.Name)).Text = "Commander "..v.Name..": Selected"
-			table.remove(troops,1)
+			SelectedTower(v,true)
 		end
 	end
 end
 game:GetService("Workspace").Towers.ChildAdded:Connect(function(v)
-	wait(.5)
-	if v.Replicator:GetAttribute("OwnerName") == game.Players.LocalPlayer.DisplayName and v.Replicator:GetAttribute("Type") == "Commander" and troops[1] then
-		v.Name = troops[1]
-		Status(tonumber(v.Name)).Text = "Commander "..v.Name..": Selected"
-		table.remove(troops,1)
+	wait(1.2)
+	if v.Replicator:GetAttribute("OwnerName") == game.Players.LocalPlayer.DisplayName and v.Replicator:GetAttribute("Type") == "Commander" then
+		SelectedTower(v,true)
 	end
 end)
 game:GetService("Workspace").Towers.ChildRemoved:Connect(function(v)
 	wait()
 	if v.Name:match("%d+") then
-		table.insert(troops, tonumber(v.Name))
-		Status(tonumber(v.Name)).Text = "Commander "..v.Name..": None"
+		SelectedTower(v,false)
 	end
 end)
-function Act(Comm)
-	if game:GetService("Workspace").Towers:FindFirstChild(tostring(Comm)) then
-		if not game:GetService("Workspace").Towers:FindFirstChild(tostring(Comm)).Replicator.Stuns:GetAttribute("1") or game:GetService("Workspace").Towers:FindFirstChild(tostring(Comm)).Replicator.Stuns:GetAttribute("1") == false then
-			game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer("Troops","Abilities","Activate",{["Troop"]=game:GetService("Workspace").Towers:FindFirstChild(tostring(Comm)),["Name"] = "Call Of Arms"})
-			Status(Comm).Text = "Commander "..Comm..": Actived"
+function Chain(Tower,Info)
+	if not Info then
+		Info = 0
+	end
+	if Tower then
+		if not Tower.Replicator.Stuns:GetAttribute("1") or Tower.Replicator.Stuns:GetAttribute("1") == false then
+			game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer("Troops","Abilities","Activate",{["Troop"]= Tower ,["Name"] = "Call Of Arms"})
+			Status(Info).Text = "Commander "..Info..": Actived"
 			wait(10.01)
-			Status(Comm).Text = "Commander "..Comm..": Waiting"
+			Status(Info).Text = "Commander "..Info..": Waiting"
 		else
-			Status(Comm).Text = "Commander "..Comm..": Stunning"
-			repeat wait() until not game:GetService("Workspace").Towers:FindFirstChild(tostring(Comm)).Replicator.Stuns:GetAttribute("1") or game:GetService("Workspace").Towers:FindFirstChild(tostring(Comm)).Replicator.Stuns:GetAttribute("1") == false
-			game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer("Troops","Abilities","Activate",{["Troop"]=game:GetService("Workspace").Towers:FindFirstChild(tostring(Comm)),["Name"] = "Call Of Arms"})
-			Status(Comm).Text = "Commander "..Comm..": Actived"
+			Status(Info).Text = "Commander "..Info..": Stunning"
+			repeat wait() 
+			until not Tower.Replicator.Stuns:GetAttribute("1") or Tower.Replicator.Stuns:GetAttribute("1") == false
+			game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer("Troops","Abilities","Activate",{["Troop"]= Tower ,["Name"] = "Call Of Arms"})
+			Status(Info).Text = "Commander "..Info..": Actived"
 			wait(10.01)
-			Status(Comm).Text = "Commander "..Comm..": Waiting"
+			Status(Info).Text = "Commander "..Info..": Waiting"
 		end	
 	else
-		Status(Comm).Text = "Commander "..Comm..": Skipping"
+		Status(Info).Text = "Commander "..Info..": Skipping"
 		wait(10.01)
-		Status(Comm).Text = "Commander "..Comm..": Waiting"
+		Status(Info).Text = "Commander "..Info..": Waiting"
 	end
 end
 while wait() do
 	if w.flags.autochain then
-		Act(1)
-		Act(2)
-		Act(3)
+		Chain(troops["Commander1"][2],troops["Commander1"][1])
+		Chain(troops["Commander2"][2],troops["Commander2"][1])
+		Chain(troops["Commander3"][2],troops["Commander3"][1])
 	end
 end
---[[for c=1,3 do
-	if game:GetService("Workspace").Towers:FindFirstChild(tostring(c)) then
-		say("WARNING","Commander"..tostring(c).." detected")
-	else
-		say("WARNING","Commander "..tostring(c).." not placed, waiting to be placed...")
-		conn = game:GetService("Workspace").Towers.ChildAdded:Connect(function(v)
-			if v.Replicator:GetAttribute("OwnerName") then 
-				if v.Replicator:GetAttribute("OwnerName") == game.Players.LocalPlayer.DisplayName and v.Replicator:GetAttribute("Type") == "Commander" then
-					cc = cc + 1
-					v.Name = cc
-				end
-			end
-		end)
-		repeat wait() until game:GetService("Workspace").Towers:FindFirstChild(tostring(c))
-		conn:Disconnect()
-	end
-end]]
